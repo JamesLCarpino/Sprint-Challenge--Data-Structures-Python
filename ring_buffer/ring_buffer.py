01011e6695a6f317185203c309529ede8b767ea8
Pinefoot <jameslcarpino@gmail.com>
2020-06-12 10:13:28 -0700

    ring buffer passing 5/6 tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,24 @@
+
+
+
 class RingBuffer:
-    def __init__(self, capacity=[]):
+    def __init__(self, capacity):
         self.capacity = capacity
+        self.data = []
+        self.current = 0
 
     def append(self, item):
-        self.capacity.append(item)
+        # need to take the first item
+        #if data length is less than the capacity add to the array
+        if len(self.data) < self.capacity:
+            self.data.append(item)
+            return self.data
+        # if the data length = the capacity then the item gets put in at index 0
+        elif len(self.data) == self.capacity:
+            self.data[self.current] = item
+            self.current += 1
+            # self.data.append(item)
+
 
     def get(self):
-        pass
+        return self.data
